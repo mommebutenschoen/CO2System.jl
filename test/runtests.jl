042aa4d265d909e1659c4e74015030f8a9c910c6
Momme Butenschön <momme.butenschon@cmcc.it>
2026-02-19 20:53:48 +0100

    Added more tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,7 +20,10 @@ using Test
         maxit = 50,
         )
     all_inputs = merge(inputs, opt_inputs)
-    #dc = (T => 18.0,S=35.0,Rho=inputs.1025.711,PO4=0.0,Si=0.0,DIC=2000.0,TA=2300.0)
+
+    # Reference output from previous versions:
+    prev_o = (CO2=10.325234153494868,HCO3=1781.9086787891802,CO3=207.76639106353602,pCO2=300.74498365961034,OmegaA=3.160654515808703,
+        OmegaC=4.882111778502531,fCO2=300.73359373048663,pH=8.143047856257516,Hplus=7.1936970405460915e-9)
 
     # Reference values from MOCSY test function:
     mocsy_i = (T=18.0,S=35.0,pr_in=100.0,Rho=1025.71105957,PO4=0.0,Si=0.0,DIC=2000.0,
@@ -33,6 +36,7 @@ using Test
         patm=1013.25,S=35.0)
     bfm_o = (HCO3=1781.9086,fCO2 =305.18082,pCO2=316.96063,OmegaC=4.882114f0,CO3=207.7665,
         CO2=10.325228,pH=8.143048,OmegaA=3.160656)
+
     function test()
         cs = CarbonateSystem(; inputs..., opt_inputs...)
 
@@ -61,7 +65,17 @@ using Test
     CS,dDIC = test()
 
     @testset "CO2System.jl" begin
+        # error checking
+        @test CS.errorflag == 0
+        # test that sum of DIC components equals total DIC concentation from input
         @test dDIC < 1.e-3
+        # test for consistency with previous versions:
+        for (k,v) in pairs(prev_o)
+            @test isapprox(CS[k],v,atol=1e-6)
+        end
+        # @test
+        # test for consistency with BFM outputs:
+
     #    @test YourPackageName.greet_your_package_name() != "Hello world!"
     end
 end
